a1b598aafc462f5dc64d60d8db7dae8d042d170e
Utkarsh Chaturvedi <utkarsh@playsimple.in>
2022-09-07 21:48:19 +0530

    Completed min coins coin change in both recurse + DP

--- DP_AV/coinChange.py
+++ DP_AV/coinChange.py
@@ -1,45 +1,77 @@
-#Have to do again
-#Have to do again
+# https://leetcode.com/problems/coin-change/submissions/
 class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        minCoins = min(coins)
-        if(amount == 0):
-            # print("target ", target, " res ", 0)
-            return 0
-        if(amount < minCoins):
-            return -1
-        
-        dp = [None for i in range(amount + 1)]
+    def coinChangeRecurseMemoised(self, coins: List[int], amount: int) -> int:
+        coins = sorted(coins)
+        dp = {}
         def helper(target):
-            # print("target ", target)
             if(target == 0):
-                # print("target ", target, " res ", 0)
                 return 0
-            
-            if(target < minCoins):
-                # print("target ", target, " res ", -1)
+            if(target < coins[0]):
                 return -1
-            
-            
-            if(dp[target] != None):
-                return dp[target] 
+            if(target in dp):
+                return dp[target]
             
             minVal = -1
-            
             for coin in coins:
+                if(coin > target):
+                    break
                 
-                valForCoin = helper(target - coin)
-                if(valForCoin >= 0):
-                    if(minVal == -1 or valForCoin < minVal):
-                        minVal = valForCoin
-            if(minVal == -1):
-                dp[target] = -1
-            else:
-                dp[target] = minVal + 1
-            # print("target ", target, " res ", dp[target])
+                coinUsedAns = helper(target - coin)
+                if(coinUsedAns == -1):
+                    continue
+                else:
+                    valCoin = coinUsedAns + 1
+                    if(minVal == -1 or minVal > valCoin):
+                        minVal = valCoin
+            dp[target] = minVal
             return dp[target]
-            
+        
         return helper(amount)
+    
+    def coinChange(self, coinsGiven: List[int], amount: int) -> int:
+        if(amount == 0):
+            return 0
+        
+        coins = []
+        for coin in coinsGiven:
+            if(coin <= amount):
+                coins.append(coin)
+        
+        n = len(coins)
+        if(n == 0):
+            return -1
+        
+        coins = sorted(coins)
+        
+        dp = [None for i in range(amount + 1)]
+        
+        dp[0] = 0
+        
+        for coin in coins:
+            if(coin < amount):
+                dp[coin] = 1
+        
+        for i in range(amount + 1):
+            if(dp[i] != None):
+                continue
+            else:
+                minValForI = -1
+                for j in coins:
+                    if(j > i):
+                        break
+                    ansJUsed = dp[i - j]
+                    if(ansJUsed == -1):
+                        continue
+                    else:
+                        ansForI = ansJUsed + 1
+                        if(minValForI == -1 or minValForI > ansForI):
+                            minValForI = ansForI
+                dp[i] = minValForI
+        
+        # print(amount, dp)
+        
+        return dp[amount]
                 
-                
+        
+            
         
