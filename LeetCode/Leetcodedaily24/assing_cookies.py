4d22b46ae7446b1c51570fc9b8940bf62ba58208
Utkarsh Chaturvedi <utkarshc0@gmail.com>
2024-01-01 19:10:13 +0530

    Adding 1st jan commit for leet code daily

---
+++ LeetCode/Leetcodedaily24/assing_cookies.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g = sorted(g)
+        s = sorted(s)
+
+        n = len(g)
+        m = len(s)
+
+        children_happy = 0
+        child= 0
+        for i in range(m):
+            cookie = s[i]
+            if child >= n:
+                break
+            if g[child] <= cookie:
+                #child happy
+                children_happy +=1
+                child +=1
+            else:
+                #useless cookie
+                pass
+        return children_happy
+
+
+            
